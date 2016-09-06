8ef39254c99a255409c7a7e8f201f5bb069952a2
Michał Jezierski <ambinanitelo@gmail.com>
2016-09-06 22:47:39 +0200

    bundling with webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    client: './dist/client.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ],
+  module: {
+
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+
+  }
+};
