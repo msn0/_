7ae3881be609748253a8302a5ff2cf42bf0f2208
Michał Jezierski <ambinanitelo@gmail.com>
2016-09-11 21:43:45 +0200

    a fix

--- src/source/strava.js
+++ src/source/strava.js
@@ -4,7 +4,7 @@ const baseUrl = 'https://www.strava.com/api/v3'
 export function getAthlete() {
   return fetch(`${url}/athletes`, {
     headers: {
-      'Authorization': 'Bearer ${accessTokenPublic}'
+      'Authorization': `Bearer ${accessTokenPublic}`
     }
   });
 };
