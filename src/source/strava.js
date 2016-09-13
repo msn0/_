cef1eb5204e858052a562b3bda08dd1a51a8a1a8
Michał Jezierski <ambinanitelo@gmail.com>
2016-09-13 20:32:13 +0200

    some fixes

--- src/source/strava.js
+++ src/source/strava.js
@@ -1,10 +1,10 @@
 const accessTokenPublic = 'c087a02680711eddadbc58c0afe18710766e024f';
-const baseUrl = 'https://www.strava.com/api/v3'
+const baseUrl = 'https://www.strava.com/api/v3';
 
 export function getAthlete() {
-  return fetch(`${url}/athletes`, {
+  return fetch(`${baseUrl}/athletes`, {
     headers: {
       'Authorization': `Bearer ${accessTokenPublic}`
     }
   });
-};
+}
