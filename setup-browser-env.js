eb2b8218ba93d198372f30e85cd0de9f886c0a5d
Michał Jezierski <ambinanitelo@gmail.com>
2016-09-14 16:02:27 +0200

    make ava specs work with less imports

--- setup-browser-env.js
+++ setup-browser-env.js
@@ -1,3 +1,14 @@
+const hook = require('css-modules-require-hook');
+const parser = require('postcss-less').parse;
+
+hook({
+  extensions: '.less',
+  generateScopedName: '[name]__[local]___[hash:base64:5]',
+  processorOpts: {
+    parser
+  }
+});
+
 global.document = require('jsdom').jsdom('<body></body>');
 global.window = document.defaultView;
 global.navigator = window.navigator;
